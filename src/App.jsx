640f44f644a0a541eb88fc75495645ed250dc18f
ferdous725890 <ferdous725890@gmail.com>
2024-11-25 22:09:07 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+
+import './App.css'
+
+function App() {
+  const handeladduser = (event) =>{
+    event.preventDefault()
+    const form = event.target
+    const name = form.name.value
+    const email = form.email.value
+    console.log(name, email);
+    const user = {name, email}
+    fetch("http://localhost:5000/users",{
+      method: 'POST',
+      headers: {
+        
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res =>res.json())
+    .then( data =>{
+      console.log("data", data );
+    } )
+  }
+  return (
+    <>
+     <h2>hello first</h2>
+     <form onSubmit={handeladduser} >
+      <input type="text" name="name" id="" /> <br></br>
+      <input type="email" name="email" id="" /> <br></br>
+      <input type="submit" value="add user" /> <br></br>
+     </form>
+    </>
+  )
+}
+
+export default App
+
+
+
